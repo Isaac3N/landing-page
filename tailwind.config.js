cbdba1928124dde9f89f8fda113b0eaa6fd7e8c7
Isaac Ndubuisi <theisaac3n@gmail.com>
2022-06-05 01:32:32 +0100

    modified screen pixels

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,15 @@
 module.exports = {
   content: ["./src/input.css", "./public/index.html", "./public/output.css"],
   theme: {
+    container: {
+      center: true,
+      padding: "1rem",
+      screen: {
+        lg: "1124px",
+        xl: "1124px",
+        "2xl": "1124px",
+      },
+    },
     extend: {},
   },
   plugins: [],
