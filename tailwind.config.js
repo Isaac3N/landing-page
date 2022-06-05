b183a94a7a3ab19e7e1dc519bfda1adc92ef2391
Isaac Ndubuisi <theisaac3n@gmail.com>
2022-06-05 01:47:25 +0100

    website theme(color)

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,13 @@
 module.exports = {
   content: ["./src/input.css", "./public/index.html", "./public/output.css"],
   theme: {
+    colors: {
+      "bookmark-purple": "#5267DF",
+      "bookmark-red": "#FA5959",
+      "bookmark-blue": "#242A45",
+      "bookmark-grey": "#9194A2",
+      "bookmark-white": "#f7f7f7",
+    },
     container: {
       center: true,
       padding: "1rem",
@@ -10,7 +17,11 @@ module.exports = {
         "2xl": "1124px",
       },
     },
+    fontFamily: {
+      Roboto: ["Roboto, sans-serif"],
+    },
     extend: {},
   },
+
   plugins: [],
 };
